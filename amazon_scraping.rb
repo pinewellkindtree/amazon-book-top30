90b9a668ffbe5d90a67c019da425dd8249030982
松井優樹 <matsuiyuuki@matsuiyuukinoMacBook-Pro.local>
2020-06-09 18:36:20 +0900

    改行を消して序列を付した

--- amazon_scraping.rb
+++ amazon_scraping.rb
@@ -1,9 +1,10 @@
 require 'mechanize'
 
 agent = Mechanize.new
-page = agent.get("https://www.amazon.co.jp/gp/bestsellers/books/ref=zg_bs_nav_0")
+page = agent.get("https://www.amazon.co.jp/gp/bestsellers/books/")
 elements = page.search('body div div div div div div ol li span div span a div')
+elements1 = elements.each_slice(2).map(&:last)
 
-elements.each do |ele|
-    puts ele.inner_text
+elements1.first(30).each_with_index do |ele,i|
+    print "第#{i+1}位:" + ele.inner_text.to_s
 end
